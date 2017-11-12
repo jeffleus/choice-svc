fa9ee56c22d66cbd1ba0ab077af0026b9132bcdd
jeffleus <jeffleus@gmail.com>
2017-11-12 08:50:43 -0800

    Init VaTech micro svc branch
    Start branch from master.
    Change db connection to FS_VATECH
    Test with index.js
    Rename service in YAML file to release

--- index.js
+++ index.js
@@ -12,26 +12,12 @@ var choice = {
 	isActive: false
 };
 
-var id = 2;
-var filter = ('1,3,5,7').split(',');
-console.log("FILTER: ", filter);
+//var id = 2;
+//var filter = ('1,3,5,7').split(',');
+//console.log("FILTER: ", filter);
 
-//Choice.get(null, filter).then(function(student) {
-//Choice.create(choice).then(function(student) {
-Choice.update(choice).then(function(student) {
-    
-//Sport.get(null, filter).then(function(student) {
-//Sport.get('XXX').then(function(student) {
-//Sport.create(sport).then(function(student) {
-//Sport.update(sport).then(function(student) {
-//Sport.delete('XXX').then(function(result) {
-	
-//Athletes.delete(1165).then(function(result) {
-//Athletes.update(ath).then(function(student) {
-//Athletes.create(ath).then(function(student) {
-//Athletes.get(id, filter).then(function(student) {
-//	console.log(result);
-	console.log(student);
+Choice.get(null, null).then(function(result) {    
+	console.log(result);
 	return;
 }).catch(function(err) {
 	console.error(err);
